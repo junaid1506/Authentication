ec072a2447c4105f9cf5b6a097f64562833335ff
junaid1506 <junnu2757@gmail.com>
2026-05-27 13:31:46 +0530

    Make utils for Files for token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const express = require("express");
+const dotenv = require("dotenv");
 const cookies = require("cookie-parser");
 const connectDB = require("./config/db");
 const app = express();
@@ -10,6 +11,7 @@ const userRoutes = require("./routes/user.routes");
 const todoRoutes = require("./routes/todo.routes");
 const authMiddleware = require("./middleware/authMiddleware");
 
+dotenv.config();
 app.use(express.json());
 app.use(cookies());
 
